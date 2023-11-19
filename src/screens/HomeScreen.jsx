85a653cafc77b0dd2748e787329fb740df8a587a
Hamed <ostovari.eng@gmail.com>
2023-11-19 11:41:56 +0400

    initial commit: config the project

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView className="h-screen items-center items-center">
+      <Text>HOME SCREEN</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
