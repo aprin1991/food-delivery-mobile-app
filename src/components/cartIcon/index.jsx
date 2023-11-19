8f6a0588a77816efd8fea666ac2f5e818ffe72e1
Hamed <ostovari.eng@gmail.com>
2023-11-19 17:15:59 +0400

    implemented cart icon ui

---
+++ src/components/cartIcon/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { themeColors } from "../../theme";
+
+const CartIcon = () => {
+  return (
+    <View className="absolute bottom-10 w-full z-50">
+      <TouchableOpacity
+        style={{ backgroundColor: themeColors.bgColor(1) }}
+        className="flex-row justify-between items-center mx-5 rounded-full p-4 py-3 shadow-lg"
+      >
+        <View
+          className="p-2 px-4 rounded-full"
+          style={{ backgroundColor: "rgba(255,255,255,0.3)" }}
+        >
+          <Text className="font-extrabold text-white text-lg">3</Text>
+        </View>
+        <Text className="flex-1 text-center font-extrabold text-white text-lg">
+          View Cart
+        </Text>
+        <Text className="font-extrabold text-white text-lg">${23}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default CartIcon;
--- src/screens/Restaurant.jsx
+++ src/screens/Restaurant.jsx
@@ -3,66 +3,68 @@ import React from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import * as Icon from "react-native-feather";
 import { ScrollView } from "react-native-gesture-handler";
+import CartIcon from "../components/cartIcon";
+import DishRow from "../components/dishRow";
 import { themeColors } from "../theme";
 
 const Restaurant = () => {
   const navigation = useNavigation();
 
   const { params } = useRoute();
-  const {
-    params: {
-      id,
-      title,
-      imgUrl,
-      rating,
-      type,
-      address,
-      description,
-      dishes,
-      lng,
-      lat,
-    },
-  } = useRoute();
   const item = params;
   return (
-    <ScrollView>
-      <View>
-        <Image className="h-72 w-full" source={item.image} />
-        <TouchableOpacity
-          onPress={() => navigation.goBack()}
-          className="absolute top-14 left-4 bg-gray-50 p-2 rounded-full shadow"
-        >
-          <Icon.ArrowLeft strokeWidth={3} stroke={themeColors.bgColor(1)} />
-        </TouchableOpacity>
-      </View>
-      <View
-        style={{ borderTopLeftRadius: 40, borderTopRightRadius: 40 }}
-        className="bg-white -mt-12 pt-6"
-      >
-        <View className="px-5">
-          <Text className="text-3xl font-bold">{title}</Text>
-          {/* copy this code from restaurant card */}
-          <View className="flex-row space-x-2 my-1">
-            <View className="flex-row items-center space-x-1">
-              <Image
-                source={require("../assets/images/fullStar.png")}
-                className="h-4 w-4"
-              />
-              <Text className="text-xs">
-                <Text className="text-green-700">{rating}</Text>
-                <Text className="text-gray-700"> (4.6k review)</Text> ·{" "}
-                <Text className="font-semibold text-gray-700">{type}</Text>
-              </Text>
-            </View>
-            <View className="flex-row items-center space-x-1">
-              <Icon.MapPin color="gray" width={15} height={15} />
-              <Text className="text-gray-800 text-xs"> Nearby · {address}</Text>
-            </View>
-          </View>
-          <Text className="text-gray-500 mt-2">{description}</Text>
+    <View>
+      <CartIcon />
+      <ScrollView>
+        <View>
+          <Image className="h-72 w-full" source={item.image} />
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            className="absolute top-14 left-4 bg-gray-50 p-2 rounded-full shadow"
+          >
+            <Icon.ArrowLeft strokeWidth={3} stroke={themeColors.bgColor(1)} />
+          </TouchableOpacity>
         </View>
-      </View>
-    </ScrollView>
+        <View
+          style={{ borderTopLeftRadius: 40, borderTopRightRadius: 40 }}
+          className="bg-white -mt-12 pt-6"
+        >
+          <View className="px-5">
+            <Text className="text-3xl font-bold">{item.name}</Text>
+            {/* copy this code from restaurant card */}
+            <View className="flex-row space-x-2 my-1">
+              <View className="flex-row items-center space-x-1">
+                <Image
+                  source={require("../assets/images/fullStar.png")}
+                  className="h-4 w-4"
+                />
+                <Text className="text-xs">
+                  <Text className="text-green-700">{item.rating}</Text>
+                  <Text className="text-gray-700"> (4.6k review)</Text> ·{" "}
+                  <Text className="font-semibold text-gray-700">
+                    {item.type}
+                  </Text>
+                </Text>
+              </View>
+              <View className="flex-row items-center space-x-1">
+                <Icon.MapPin color="gray" width={15} height={15} />
+                <Text className="text-gray-800 text-xs">
+                  {" "}
+                  Nearby · {item.address}
+                </Text>
+              </View>
+            </View>
+            <Text className="text-gray-500 mt-2">{item.description}</Text>
+          </View>
+          <View className="bg-white pb-36">
+            <Text className="px-4 py-4 text-2xl font-bold">Menu</Text>
+            {item.dishes.map((dish, index) => {
+              return <DishRow key={index} dish={dish} />;
+            })}
+          </View>
+        </View>
+      </ScrollView>
+    </View>
   );
 };
 
